0c4f5b516c23915811f2a47dfd139ca18d06e083
Ramesh <ramesh.crs96@gmail.com>
2021-01-22 18:41:49 +0530

    added stack navigation

--- App.js
+++ App.js
@@ -3,9 +3,19 @@ import {View, ActivityIndicator} from 'react-native';
 import Login from './screens/LoginScreen';
 import Products from './screens/Products';
 import Start from './screens/StartScreen';
+import {NavigationContainer, StackActions} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
 const App = () => {
-  return <Start />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Welcome" component={Start} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Products" component={Products} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
