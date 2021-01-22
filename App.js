eeec22f0156f40852845b413be46e8e0d9179a3c
Ramesh <ramesh.crs96@gmail.com>
2021-01-22 14:59:53 +0530

    added products screen

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {View, ActivityIndicator} from 'react-native';
+import Products from './screens/Products';
+
+const App = () => {
+  return <Products />;
+};
+
+export default App;
---
+++ screens/Products.js
@@ -0,0 +1,71 @@
+import React, {useEffect, useState} from 'react';
+import {
+  SafeAreaView,
+  ActivityIndicator,
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+} from 'react-native';
+
+const Products = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://6wavrrn6r7.execute-api.us-west-2.amazonaws.com/products')
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        setData(json.Items);
+      })
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <View style={styles.container}>
+        {isLoading ? (
+          <ActivityIndicator />
+        ) : (
+          <FlatList
+            data={data}
+            keyExtractor={({id}, index) => id}
+            renderItem={({item}) => (
+              <View style={[styles.container]}>
+                <Text style={styles.title}>{item.name}</Text>
+                <Image
+                  style={{width: 300, height: 300}}
+                  source={{
+                    uri: item.imageUrl,
+                  }}
+                />
+              </View>
+            )}
+          />
+        )}
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    borderWidth: 1,
+    borderColor: '#20232a',
+    borderRadius: 6,
+    padding: 24,
+    margin: 5,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginTop: 16,
+    textAlign: 'center',
+  },
+});
+
+export default Products;
